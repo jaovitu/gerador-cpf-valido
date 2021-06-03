880bc53312e704800b9a8b271d1adc087fd897d0
JvGomess <vg913193@gmail.com>
2021-06-02 22:49:23 -0300

    Adicionando botão de copiar

--- src/main.js
+++ src/main.js
@@ -1,15 +1,47 @@
 import GenerateCPF from "./GenerateCPF";
 import "../public/css/style.css";
 
-(function() {
-    const cpf = new GenerateCPF();
-    const span = document.querySelector(".cpf");
-    const generate = document.querySelector(".generate");
+const cpf = new GenerateCPF();
+const span = document.querySelector(".cpf");
+const generate = document.querySelector(".generate");
 
-    span.innerHTML = cpf.generate();
 
-    generate.addEventListener( "click", () => {
-        const newCPF = cpf.generate();
-        span.innerHTML = newCPF;
-    } )
-})()
+function copyButtonConfig() {
+    const copyButton = document.querySelector(".copy-icon");
+    const copyInfoCard = document.querySelector(".copy-info");
+
+    copyButton.addEventListener("click", event => {
+        const textInput = document.createElement("input");
+        textInput.value = span.textContent;
+
+        document.body.appendChild(textInput);
+
+        textInput.select()
+        document.execCommand("copy");
+
+        document.body.removeChild(textInput);
+
+        copyInfoCard.style.marginRight = "158.5px";
+        copyInfoCard.style.backgroundColor = "rgb(140, 230, 140)";
+        copyInfoCard.textContent = "Copiado!";
+    });
+
+    copyButton.addEventListener("mouseover", event => {
+        copyInfoCard.textContent = "Clique para copiar";
+        copyInfoCard.style.visibility = "visible";
+        copyInfoCard.style.marginRight = "132px";
+    });
+
+    copyButton.addEventListener("mouseleave", event => {
+        copyInfoCard.style.visibility = "hidden";
+        copyInfoCard.style.backgroundColor = "cadetblue";
+    })
+}
+
+span.innerHTML = cpf.generate();
+copyButtonConfig();
+
+generate.addEventListener( "click", () => {
+    const newCPF = cpf.generate();
+    span.innerHTML = newCPF;
+} );
